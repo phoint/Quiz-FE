97b10f608eba4f990738c56d32f134941b09d10a
phoint <phoi.nt2806@gmail.com>
2022-01-25 18:16:23 +0700

    Initial commit

---
+++ src/main/java/com/fa/training/group01/config/QuizAppDispatcherServletInitializer.java
@@ -0,0 +1,29 @@
+package com.fa.training.group01.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class QuizAppDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { AppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
+
+
+
+
+
+
